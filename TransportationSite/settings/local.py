c659b602f28f2387562f4cfa04d4a14d70658ee9
yukish <iwan.jukisch@gmail.com>
2019-09-12 04:23:20 +0300

    adding PostgreeSQL, setting file splitting

---
+++ TransportationSite/settings/local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'transportation',
+        'USER': 'insayrexpass',
+        'PASSWORD': 'qwerty123',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+    }
+}
